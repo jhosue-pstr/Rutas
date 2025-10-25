b46c9e8597c6c62efff80bc6ce8a5defe09a5879
jhosue-pstr <pastor.jhosue07@gmail.com>
2025-10-25 17:56:53 -0500

    aggreglos 12

--- Backend/models/__init__.py
+++ Backend/models/__init__.py
@@ -2,3 +2,14 @@ from .ruta import Ruta, RutaCreate, RutaPublic, RutaUpdate
 from .punto_ruta import PuntoRuta, PuntoRutaCreate, PuntoRutaPublic, PuntoRutaUpdate
 from .chofer import Chofer, ChoferCreate, ChoferPublic, ChoferUpdate
 from .bus import Bus, BusCreate, BusPublic, BusUpdate
+
+# Rebuild para resolver forward references
+from .ruta import RutaPublic
+from .punto_ruta import PuntoRutaPublic
+from .chofer import ChoferPublic
+from .bus import BusPublic
+
+RutaPublic.model_rebuild()
+PuntoRutaPublic.model_rebuild() 
+ChoferPublic.model_rebuild()
+BusPublic.model_rebuild()
